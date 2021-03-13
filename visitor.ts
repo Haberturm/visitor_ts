e4a9a345df87915e267e0f95bf48b5774a6be04c
Haberturm <67369502+Haberturm@users.noreply.github.com>
2021-03-13 12:16:13 +0200

    Create visitor.ts

---
+++ visitor.ts
@@ -0,0 +1,60 @@
+
+class BMW {
+    startEngine() {
+        console.log('wwrrrrr')
+    }
+ 
+    accept(operation:Visitor) {
+        operation.visitBMW(this)
+    }
+}
+ 
+class Mercedes {
+    startEngine() {
+        console.log('drdrrrr')
+    }
+ 
+    accept(operation:Visitor) {
+        operation.visitMercedes(this)
+    }
+}
+ 
+class Toyota {
+    startEngine() {
+        console.log('Rrrrrrr')
+    }
+ 
+    accept(operation:Visitor) {
+        operation.visitToyota(this)
+    }
+}
+ 
+
+
+ 
+class Visitor {
+    visitBMW(bmw:BMW){
+        bmw.startEngine()
+    }
+    visitMercedes(mercedes:Mercedes){
+        mercedes.startEngine()
+    }
+    visitToyota(dolphin:Toyota){
+        dolphin.startEngine()
+    }
+}
+ 
+
+ 
+ 
+const bmw = new BMW()
+const mercedes = new Mercedes()
+const toyota = new Toyota()
+const start = new Visitor();
+
+ 
+ 
+bmw.accept(start)           // wwrrrrr
+mercedes.accept(start)      // drdrrrr
+toyota.accept(start)        // Rrrrrrr
+
